6fd5c7c107c0bd63d258da57d7323b42d6070c22
desan <desanctisisabella95@gmail.com>
2025-07-27 17:21:06 +0200

    created User and SalaryController

--- src/main/java/com/develhope/ControllerProtection/controllers/SalaryController.java
+++ src/main/java/com/develhope/ControllerProtection/controllers/SalaryController.java
@@ -1,4 +1,14 @@
 package com.develhope.ControllerProtection.controllers;
 
+import com.develhope.ControllerProtection.repositories.SalaryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
 public class SalaryController {
+
+    @Autowired
+    private SalaryRepository salaryRepo;
 }
--- src/main/java/com/develhope/ControllerProtection/controllers/UserController.java
+++ src/main/java/com/develhope/ControllerProtection/controllers/UserController.java
@@ -1,9 +1,17 @@
 package com.develhope.ControllerProtection.controllers;
 
+import com.develhope.ControllerProtection.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/users")
 public class UserController {
+
+//    @GetMapping
+    @Autowired
+    private UserRepository userRepo;
+
 }
