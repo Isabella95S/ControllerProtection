08053ed95ddf239c6169bdee1010626ddeef8d65
desan <desanctisisabella95@gmail.com>
2025-07-28 22:30:55 +0200

    created SecurityConfig class

--- src/main/java/com/develhope/ControllerProtection/auth/SecurityConfig.java
+++ src/main/java/com/develhope/ControllerProtection/auth/SecurityConfig.java
@@ -1,4 +1,47 @@
 package com.develhope.ControllerProtection.auth;
 
+import com.develhope.ControllerProtection.services.DatabaseUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private DatabaseUserDetailsService service;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, DatabaseUserDetailsService userDetailsService)
+            throws Exception{
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/hello").permitAll()
+                .requestMatchers("/users/**").hasAnyRole("USER", "ADMIN")
+                    .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                .permitAll()
+            )
+            .logout(logout -> logout.permitAll());
+
+        http.userDetailsService(userDetailsService);
+        return http.build();
+
+    }
+
 }
