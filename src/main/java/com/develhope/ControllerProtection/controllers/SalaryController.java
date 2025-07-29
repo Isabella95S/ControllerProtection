e34fa3cf38c463dd85bfbd67a9af36b10e8cde63
desan <desanctisisabella95@gmail.com>
2025-07-29 18:39:21 +0200

    fixed application.properties

--- src/main/java/com/develhope/ControllerProtection/controllers/SalaryController.java
+++ src/main/java/com/develhope/ControllerProtection/controllers/SalaryController.java
@@ -1,14 +1,51 @@
 package com.develhope.ControllerProtection.controllers;
 
+import com.develhope.ControllerProtection.models.Salary;
+import com.develhope.ControllerProtection.models.User;
 import com.develhope.ControllerProtection.repositories.SalaryRepository;
+import com.develhope.ControllerProtection.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("salary")
 public class SalaryController {
 
     @Autowired
     private SalaryRepository salaryRepo;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public Salary getSalary(@PathVariable Integer id) {
+        Optional<Salary> salaryOptional = salaryRepo.findById(id);
+        if (salaryOptional.isEmpty()) {
+            return salaryOptional.get();
+        }else{
+            return  null;
+        }
+    }
+
+    public Salary findByUser(@PathVariable("id") Integer id) {
+        Optional<User> optional = userRepository.findById(id);
+        if (optional.isEmpty()) {
+            return optional.get().getSalary();
+        }else{
+            return null;
+        }
+    }
+    @GetMapping("/salaries")
+    public List<Salary> getAllSalaries() {
+        return salaryRepo.findAll();
+    }
+
+    @PostMapping("")
+    public Salary create(@RequestBody Salary salary) {
+
+        return salaryRepo.save(salary);
+    }
 }
