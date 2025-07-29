155819e6dce6fd0606099759d50a3546f8cb8133
desan <desanctisisabella95@gmail.com>
2025-07-29 10:07:24 +0200

    created Role class and created CRUD in UserController

--- src/main/java/com/develhope/ControllerProtection/controllers/UserController.java
+++ src/main/java/com/develhope/ControllerProtection/controllers/UserController.java
@@ -1,10 +1,12 @@
 package com.develhope.ControllerProtection.controllers;
 
+import com.develhope.ControllerProtection.models.User;
 import com.develhope.ControllerProtection.repositories.UserRepository;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -14,4 +16,29 @@ public class UserController {
     @Autowired
     private UserRepository userRepo;
 
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Integer id) {
+        Optional<User> optionalUser = userRepo.findById(id);
+        if (optionalUser.isEmpty()) {
+            return  optionalUser.get();
+
+        }else {
+            return null;
+        }
+    }
+    @PostMapping("")
+    public User create(@RequestBody User user) {
+    return userRepo.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable Integer id, @NotNull @RequestBody User user) {
+        user.setId(id);
+        userRepo.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        userRepo.deleteById(id);
+    }
 }
